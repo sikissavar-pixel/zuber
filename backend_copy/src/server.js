abd84cebf7c635a59b04334a7bff1d88e7704c12
sikissavar-pixel <sikissavar@gmail.com>
2025-11-24 02:48:26 +0300

    Railway: set root to backend_copy and use local Dockerfile; server routes handled in app.js

--- backend_copy/src/server.js
+++ backend_copy/src/server.js
@@ -2,9 +2,6 @@ import { createServer } from "http";
 import app from "./app.js";
 import { initDb, ensureAdmin } from "./config/db.js";
 import { initSocket } from "./sockets/index.js";
-import applyRoutes from "./routes/apply.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import partnerRoutes from "./routes/partner.routes.js";
 
 const PORT = parseInt(process.env.PORT || "3001", 10);
 
@@ -12,15 +9,7 @@ const httpServer = createServer(app);
 const io = initSocket(httpServer);
 app.set("io", io);
 
-// Route'ları bağla
-app.use("/api/applications", applyRoutes);
-console.log("Route yüklendi: /api/applications");
-
-app.use("/api/users", userRoutes);
-console.log("Route yüklendi: /api/users");
-
-app.use("/api/partners", partnerRoutes);
-console.log("Route yüklendi: /api/partners");
+// Route'lar app.js içinde bağlanır
 
 initDb()
   .then(() => ensureAdmin())
